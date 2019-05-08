e8e80fe383ab792d75242ab087e445d198749dc5
Vijay Gawade <vijay.gawade@globant.com>
2019-05-08 20:09:02 +0530

    Remove duplicates from array

--- Algorithms/factorial.js
+++ Algorithms/factorial.js
@@ -5,3 +5,18 @@ function factorial(num) {
   return num * factorial(num - 1);
 }
 factorial(10);
+
+var array2 = [
+  { name: 'A', age: 23 },
+  { name: 'B', age: 23 },
+  { name: 'C', age: 23 },
+  { name: 'A', age: 23 }
+];
+var filteredDecords = [];
+var newArray = [];
+array2.forEach((e, i) => {
+  if (!newArray.indexOf(e.name)) {
+    newArray.push(e.name);
+  }
+});
+console.log(newArray);
